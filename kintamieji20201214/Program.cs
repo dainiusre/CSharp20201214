aac50ab65f7323fedb47cab6843f20da22b74c18
Juspus <juspus@users.noreply.github.com>
2020-12-14 21:50:26 +0200

    kintamieji ir console

--- kintamieji20201214/Program.cs
+++ kintamieji20201214/Program.cs
@@ -22,33 +22,28 @@ namespace kintamieji20201214
             string studijuPav = "Robotika";
             int kredituSk = 90;
 
-            //string veiksmas = "Nupirkti pieno";
+            string veiksmas = "Nupirkti pieno";
 
-            //Console.WriteLine("Justo Programa:");
-            //Console.WriteLine("1. " + "Pavedzioti suni");
-            //Console.Write("2" + ". " + veiksmas + "\n");
-            //Console.WriteLine("3. Eiti paprogramuot");
-            //Console.ReadLine();
+            Console.WriteLine("Justo Programa:");
+            Console.WriteLine("1. " + "Pavedzioti suni");
+            Console.Write("2" + ". " + veiksmas + "\n");
+            Console.WriteLine("3. Eiti paprogramuot");
+            Console.ReadLine();
 
-            //Console.WriteLine("STUDENTO DUOMENYS");
-            //Console.WriteLine($"{vardas} {pavarde} ({amzius}m.)");
-            //Console.WriteLine("Jo ugis yra {0} m., jo svoris {1} kg. jis zino, kad sveria {1} ir zino, kad jo ugis {0}", ugis, svoris);
-            //Console.WriteLine("Jis mokosi auksotjoje mokykloje: " + aukstojiMokykla + " (" + studijuPav + ")");
-            //Console.WriteLine("Jis siuo metu yra baiges {0} ir mokosi {1}", kursas-1, kursas);
-            //Console.ReadLine();
+            Console.WriteLine("STUDENTO DUOMENYS");
+            Console.WriteLine($"{vardas} {pavarde} ({amzius}m.)");
+            Console.WriteLine("Jo ugis yra {0} m., jo svoris {1} kg. jis zino, kad sveria {1} ir zino, kad jo ugis {0}", ugis, svoris);
+            Console.WriteLine("Jis mokosi auksotjoje mokykloje: " + aukstojiMokykla + " (" + studijuPav + ")");
+            Console.WriteLine("Jis siuo metu yra baiges {0} ir mokosi {1}", kursas - 1, kursas);
+            Console.ReadLine();
 
             Console.WriteLine("Iveskite kazka:");
             string ivestis = Console.ReadLine();
             Console.WriteLine($"Ivede: {ivestis}");
             Console.WriteLine("Iveskite sveika skaiciu: ");
 
-            int sk;
-            if(int.TryParse(ivestis, out sk))
-            {
-
-            }
             int sk1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Ivestas skaicius plius vienas " + (sk+1));
+            Console.WriteLine("Ivestas skaicius plius vienas " + (sk1));
             Console.ReadLine();
         }
     }
