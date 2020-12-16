8b600ab35055774cddabd2909e1e88fc5676c91f
Juspus <juspus@users.noreply.github.com>
2020-12-16 19:09:49 +0200

    console read

---
+++ ConsoleIsvestis2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleIsvestis2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite varda:");
+            string vardas = Console.ReadLine();
+            Console.WriteLine("Spalvos raide:");
+            //char spalvosRaide1 = Convert.ToChar(Console.ReadLine());
+            char spalvosRaide = Convert.ToChar(Console.ReadKey().KeyChar);
+            Console.WriteLine();
+            Console.WriteLine("Pirmas sk: ");
+            double sk1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Antras sk: ");
+            double sk2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{vardas} mestamiausia spalva: {spalvosRaide}, {sk1}, {sk2}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ consoleIvestis/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consoleIvestis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite kazkoki teksta:");
+            string sakinys = Console.ReadLine();
+
+            Console.WriteLine("Irasytas tekstas:\n"+sakinys);
+            /*
+            Console.WriteLine("Irasytas tekstas:");
+            Console.WriteLine(sakinys);
+            */
+
+            //Console.WriteLine($"Irasytas tekstas: \n{sakinys}");
+
+            //Console.WriteLine("Irasytas tekstas : \n{0}", sakinys);
+            Console.Read();
+        }
+    }
+}
