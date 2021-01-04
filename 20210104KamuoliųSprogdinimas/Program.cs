d7d3b268c4819f155bc93ff963172403dd33eae3
Juspus <juspus@users.noreply.github.com>
2021-01-04 21:35:57 +0200

    Spalvos Kamuoliai

---
+++ 20210104KamuoliųSprogdinimas/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _20210104KamuoliųSprogdinimas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Spalva raudona = new Spalva(255, 0, 0);
+            raudona.ColorDetails();
+            Spalva.KokiaCiaSpalva(raudona);
+
+
+            Spalva spl = Spalva.Parse("R 20 G 0 B 0");
+            spl.ColorDetails();
+            Console.Read();
+
+
+        }
+    }
+
+    class Spalva
+    {
+        public int R { get; }
+        public int G { get; }
+        public int B { get; }
+        public int A { get; }
+
+        public Spalva(int red, int green, int blue, int alpha)
+        {
+            if(InPossibleColorRange(red))
+            {
+                R = red;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            if (InPossibleColorRange(green))
+            {
+                G = green;
+            }
+            if(InPossibleColorRange(blue))
+            {
+                B = blue;
+            }
+            if(InPossibleColorRange(alpha))
+            {
+                A = alpha;
+            }
+        }
+        public Spalva(int red, int green, int blue)
+        {
+            R = PralaidusColorRange(red);
+            G = PralaidusColorRange(green);
+            B = PralaidusColorRange(blue);
+            A = 255;
+        }
+
+        public Spalva()
+        {
+            R = 0;
+            G = 0;
+            B = 0;
+            A = 0;
+        }
+
+        public void ColorDetails()
+        {
+            Console.WriteLine($"R {R} G {G} B {B} A {A}");
+        }
+
+        private bool InPossibleColorRange(int value)
+        {
+            if(value<0 || value>255)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private int PralaidusColorRange(int value)
+        {
+            if(value<0)
+            {
+                return 0;
+            }
+            if(value>255)
+            {
+                return 255;
+            }
+            return value;
+        }
+
+        public static void KokiaCiaSpalva(Spalva spl)
+        {
+            spl.ColorDetails();
+        }
+
+        public static Spalva Parse(string spalva)
+        {
+            Regex re = new Regex("R (\\d*) G (\\d*) B (\\d*)");
+            var match = re.Match(spalva);
+            var red = int.Parse(match.Groups[1].Value);
+            return new Spalva(red, 0, 0);            
+        }
+    }
+
+    class Kamuolys
+    {
+        public Spalva KamuolioSpalva { get; }
+        public double Skersmuo { get; private set; }
+        public int MetimuSkaicius { get; set; }
+        public bool Sproges { get; private set; }
+    }
+}
---
+++ 20210104Klases/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20210104Klases
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Automobilis manoAutomobilis = new Automobilis("Audi", "A4", 2005);
+            Automobilis auto1 = new Automobilis("Mazda", "3", 2010);
+            Automobilis auto2 = new Automobilis("Tesla", "Model 3");
+            Automobilis auto3 = new Automobilis();
+            auto3.InformacijaApieAutomobili();
+            manoAutomobilis.InformacijaApieAutomobili();
+            auto1.InformacijaApieAutomobili();
+            auto2.InformacijaApieAutomobili();
+
+            Console.Read();
+        }
+    }
+
+    class Automobilis
+    {
+        public string Marke;
+        public string Modelis;
+        public int GamybosMetai;
+        public DateTime GamybosData;
+
+        public Automobilis()
+        {
+
+        }
+
+        public Automobilis(string marke, string modelis, int gamybosMetai)
+        {
+            Marke = marke;
+            Modelis = modelis;
+            GamybosMetai = gamybosMetai;
+        }
+        public Automobilis(string marke, string modelis)
+        {
+            Marke = marke;
+            Modelis = modelis;
+            GamybosMetai = 2020;
+        }
+
+        public void InformacijaApieAutomobili()
+        {
+            Console.WriteLine($"Marke: {Marke}, Modelis: {Modelis}, GamybosMetai: {GamybosMetai}");
+        }
+    }
+}
